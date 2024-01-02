35f6ef03ee6f574112cb96c470da3343f6705f2f
Theresa Tran <theresa1194@gmail.com>
2024-01-02 12:21:19 +1100

    added svg icon to scroll to top

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import "../Styles/NavBar.css";
-
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 
 function NavBar() {
-  
-
   const [isSticky, setIsSticky] = useState(false);
 
   useEffect(() => {
@@ -18,12 +16,23 @@ function NavBar() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
+  function handleScrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }
+
   return (
     <div className="body">
       <nav
         className="navbar"
         style={{ position: isSticky ? "fixed" : "relative", top: 0 }}
       >
+        <div className="scroll-to-top">
+          <FontAwesomeIcon icon={faArrowUp} onClick={handleScrollToTop} />
+          <p>Back to top</p>
+        </div>
         <ul>
           <li>
             <NavLink to="/" activeClassName="active">
